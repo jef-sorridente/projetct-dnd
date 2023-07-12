f0898b2f4aa20d5a5862d513c002e6efea8d6492
Jeferson Soares <jpadilha53@gmail.com>
2023-07-12 18:19:40 -0300

    Projetc-dnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import DraggableList from "react-draggable-list";
+import "./App.css";
 
 function App() {
+  const myList = [
+    { id: 1, name: "one", color: "green" },
+    { id: 2, name: "two", color: "blue" },
+    { id: 3, name: "three", color: "red" },
+    { id: 4, name: "four", color: "lightgreen" },
+    { id: 5, name: "five", color: "lightblue" },
+    { id: 6, name: "six", color: "cyan" },
+  ];
+  const handleClick = () => {
+    console.log("mylist: ", myList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div style={{ width: 300, margin: "0 auto" }}>
+          <DraggableList width={100} height={100} itemKey="id">
+            {myList.map((item, i) => (
+              <li>
+                <div
+                  key={item.id}
+                  style={{ width: 100, height: 100, background: item.color }}
+                >
+                  {item.name}
+                </div>
+              </li>
+            ))}
+          </DraggableList>
+          <button onClick={handleClick}>get order</button>
+        </div>
+      </div>
+    </>
   );
 }
 
